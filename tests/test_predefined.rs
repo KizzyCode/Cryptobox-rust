6b8f7ea62cd99dc5e70475e2eec1a2975e5cddf1
Keziah Biermann <development@kizzycode.de>
2019-01-27 16:02:52 +0100

    Removed some pre-defined test files to make the crate smaller than 10MiB

--- tests/test_predefined.rs
+++ tests/test_predefined.rs
@@ -2,29 +2,17 @@ use std::{ io::{ Read, Write }, process::{ Command, Stdio } };
 
 
 /// The keys used for the predefined ciphertexts
-const KEYS: [&str; 8] = [
+const KEYS: [&str; 2] = [
 	"3756cc36-770620df-aa453631-6dcae688-80f5dafd-804a8739-2984a520-51eef1af",
-	"b0c76492-b09fada6-4229bb2c-d710f42d-80054860-d8fcaf3e-4805a1df-e8ff122a",
-	"78bd40ec-014338a7-ef9ccf7a-7de9a917-057d8a10-a5ab4ebb-16289c2d-88d120d5",
-	"9d700468-594c4426-efc177ef-f480373d-c1d724a5-092c64e5-ad78b4db-7de807a5",
-	"81f514b1-1745a91d-04b90403-f6f5dba9-86d2de8b-6b8190ef-bc74e528-5dd375b9",
-	"1203d773-bb9acf3b-08f28aa0-5271b26d-939bf8df-9a795ff9-e3c1f76b-8de5da12",
-	"e3051926-a33d25ae-e81dd789-c2ab505f-8ac1900f-b8280c68-e13dc629-a1c19eec",
-	"8db9d378-f77f471f-9183738c-8cd776ba-59e6bce7-7bac62c1-23cff5b2-0e3cccab"
+	"875b78a0-46265b48-7e81e5f0-51c8f09d-ac431aa5-6e76d2b6-643abc0e-4d783b27"
 ];
 /// The predefined plaintexts
-const PLAINTEXTS: [&[u8]; 8] = [
-	include_bytes!("data.0.plaintext"), include_bytes!("data.1.plaintext"),
-	include_bytes!("data.2.plaintext"), include_bytes!("data.3.plaintext"),
-	include_bytes!("data.4.plaintext"), include_bytes!("data.5.plaintext"),
-	include_bytes!("data.6.plaintext"), include_bytes!("data.7.plaintext")
+const PLAINTEXTS: [&[u8]; 2] = [
+	include_bytes!("data.0.plaintext"), include_bytes!("data.1.plaintext")
 ];
 /// The predefined ciphertexts
-const CIPHERTEXTS: [&[u8]; 8] = [
-	include_bytes!("data.0.ciphertext"), include_bytes!("data.1.ciphertext"),
-	include_bytes!("data.2.ciphertext"), include_bytes!("data.3.ciphertext"),
-	include_bytes!("data.4.ciphertext"), include_bytes!("data.5.ciphertext"),
-	include_bytes!("data.6.ciphertext"), include_bytes!("data.7.ciphertext")
+const CIPHERTEXTS: [&[u8]; 2] = [
+	include_bytes!("data.0.ciphertext"), include_bytes!("data.1.ciphertext")
 ];
 
 
